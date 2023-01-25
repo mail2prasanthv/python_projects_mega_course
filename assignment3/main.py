4c6b089a33d7d4c7b407fea441d835710669b3b3
Prasanth Vaghese <mail2prasanthv@gmail.com>
2023-01-25 17:34:38 +0530

    Assignemnt 3: Datatypes and List

---
+++ assignment3/main.py
@@ -0,0 +1,15 @@
+#List data type
+todo1 = input("Enter a todo:")
+todo2 = input("Enter a todo:")
+todo3 = input("Enter a todo:")
+
+todos = [todo1,todo2, todo3];#list
+
+print("ToDos List:"  )
+print(todos)
+
+print("Data Type: todos : " )
+print(type(todos))#list
+print("Data Type: todo1 : " )
+print(type(todo1))#string
+
