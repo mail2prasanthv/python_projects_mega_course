c750b44c7cd98d0b3f924101dd350fc08493037e
Prasanth Vaghese <mail2prasanthv@gmail.com>
2023-01-25 16:32:52 +0530

    #Read an input from user

---
+++ assignment2/main.py
@@ -0,0 +1,8 @@
+#Read an input from user - option 1
+print("Enter a todo:")
+user_text = input()
+print(user_text)
+
+#Read an input from user - option 2
+user_text = input("Enter a todo again :")
+print(user_text)
