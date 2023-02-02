7ccfb26bc9654a5dd0d222cc1b85c7459507e79f
Prasanth Vaghese <mail2prasanthv@gmail.com>
2023-02-02 16:49:10 +0530

    Section 11: Day 11 - App 1️⃣ (Todo List App) #custom-functions

---
+++ day-11/assignment27/main.py
@@ -0,0 +1,17 @@
+#user-defined functions
+
+def sum(data1,data2):
+    return data1+ data2
+
+num1_str = input("Enter first number:")
+num2_str = input("Enter second number:")
+
+data1 = int(num1_str)
+data2 = int(num2_str)
+
+sum_num = sum(data1,data2)
+print("sum is:", sum_num)
+
+
+
+
---
+++ day-11/assignment28/main.py
@@ -0,0 +1,19 @@
+#user-defined functions
+#turn none
+
+def sum(data1,data2):
+    return #not returning anything
+    #return data1+ data2
+
+num1_str = input("Enter first number:")
+num2_str = input("Enter second number:")
+
+data1 = int(num1_str)
+data2 = int(num2_str)
+
+sum_num = sum(data1,data2)
+print("sum is:", sum_num)#None
+
+
+
+
